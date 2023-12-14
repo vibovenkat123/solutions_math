d38c4ca5c23606239bfae9899b611e7c83f67565
vibovenkat123 <vibovenkat@gmail.com>
2023-12-13 18:40:44 -0600

    feat: matrix reduced row echelon

--- lin_alg_prog/matrix_manip.py
+++ lin_alg_prog/matrix_manip.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 import sympy as sp
 
-a = np.matrix([[1, -1j], [0, 2 - 2j], [0, 1+0j]])
-rrefed = sp.Matrix(a).rref()
-
-print(rrefed)
+a = np.matrix([[1, -2, 2], [0, 2, -2], [1, -3, 4]])
+b = np.matrix([[1], [-1], [2]])
+x = scipy.linalg.solve(a, b);
+print(x)
